27ab246836b4c5b52088d574ccda5c5637b33556
Phillip Nguyen <png9981@gmail.com>
2024-08-20 23:29:12 +0700

    feat: adding max redirect to 0

--- src/libs/http-client/http-client.service.ts
+++ src/libs/http-client/http-client.service.ts
@@ -6,8 +6,10 @@ export class HttpClient {
   async isValidUrl(url: string): Promise<boolean> {
     try {
       const response = await axios.get(url, {
+        maxRedirects: 0,
         timeout: 5000,
       });
+
       return response.status >= 200 && response.status < 300;
     } catch (error) {
       return false;
